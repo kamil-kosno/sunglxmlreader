dd69888fcfcbf4c9ae77a2a7a942dd7c168e07ef
kamil-kosno <kkosno@yahoo.co.uk>
2022-12-26 14:51:40 +0000

    change session id

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask, request, send_file
+from flask import render_template, Flask, request, send_file, session
 from fileoperations import FileOperations
 import datetime
 import uuid
@@ -19,14 +19,16 @@ app.secret_key = get_secret()
 
 @app.route('/')
 def home():  
-    fo = FileOperations(app.name)
+    if 'id' not in session:
+        session['id'] = get_guid()
+    fo = FileOperations(session['id'])
     fo.get_root()  
     current_year = datetime.datetime.now().year
     return render_template('index.html', footer_year=current_year)
 
 @app.route('/update_log', methods=['GET'])
 def update_log():
-    fo = FileOperations(app.name)  
+    fo = FileOperations(session['id'])  
     fo.write_log()
     return 'log written'   
 
@@ -34,12 +36,12 @@ def update_log():
 @app.route('/uploader', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        fo = FileOperations(app.name)
+        fo = FileOperations(session['id'])
         fo.clean_temp_folder()
         f = request.files['sungl_response_file']
         file_path = fo.get_file_path(f.filename)
         f.save(file_path)
-        app.config['DOWNLOAD_FILE_PATH'] = (f.filename + '_rejected.csv')
+        session['download_file_path'] = (f.filename + '_rejected.csv')
         fo.parse_xml(file_path)
         header_df = fo.get_header()
         payload_df = fo.get_payload()
@@ -54,8 +56,8 @@ def upload_file():
 
 @app.route('/download', methods=['GET'])
 def download():
-    fo = FileOperations(app.name)
-    return send_file(fo.get_combined_file_path(), download_name=app.config['DOWNLOAD_FILE_PATH'])
+    fo = FileOperations(session['id'])
+    return send_file(fo.get_combined_file_path(), download_name=session['download_file_path'])
 
 
 if __name__ == '__main__':
